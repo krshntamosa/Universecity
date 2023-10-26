8efffcb8a3a478fae233658b1607dbebe1f3c329
krshntamosa <kcarlatamosa@gmail.com>
2023-10-26 20:14:26 +0800

    bug-fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import "./App.css";
 import Login from "./Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,20 +11,4 @@ const App = () => {
     </BrowserRouter>
   );
 };
-=======
-import Splash from "./Splash/Splash";
-
-const App = () => {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  return <div>{loading ? <Splash loading="loading" /> : <p>Helloww</p>}</div>;
-};
-
->>>>>>> 829a5672931362b7274f3fe02ac3a064d5bee9fb
 export default App;
--- src/Splash/Splash.js
+++ src/Splash/Splash.js
@@ -1,9 +1,6 @@
 import React from "react";
 import styles from "./Splash.module.css";
-<<<<<<< HEAD
 import Image from "react-bootstrap/Image";
-=======
->>>>>>> 829a5672931362b7274f3fe02ac3a064d5bee9fb
 
 const Splash = (loading) => {
   return (
@@ -15,15 +12,6 @@ const Splash = (loading) => {
             src="/images/UVCLogo.png"
             alt="show-logo"
           />
-<<<<<<< HEAD
-=======
-          <BarLoader
-            className={styles["center-loader"]}
-            color={"#5ECBF0"}
-            loading={loading}
-            size={15}
-          />
->>>>>>> 829a5672931362b7274f3fe02ac3a064d5bee9fb
         </div>
         <div className={styles["loading-activity"]}></div>
       </div>
