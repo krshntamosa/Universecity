021456ea47a534fc3b04d9bacc3c74f504b44e1e
Renz <johnrenz.valencia@cit.edu>
2023-12-20 23:39:24 +0800

    FIXED

--- src/ProfileView/ProfileView.js
+++ src/ProfileView/ProfileView.js
@@ -5,6 +5,7 @@ import { Button, Form, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { auth, database } from "../firebase";
 import { getDatabase, ref, child, get } from "firebase/database";
+import { updatePassword } from "firebase/auth";
 
 
 
@@ -12,45 +13,66 @@ import { getDatabase, ref, child, get } from "firebase/database";
 
 const ProfileView = () => {
 
-  const [userData, setUserData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    university: "",
-    collegeDepartment: "",
-    collegeProgram: "",
-    password: "",
-  });
+ 
 
 
   const [isExpanded, setExpanded] = useState(false);
   const [showText, setShowText] = useState(false);
+  const [password, setPassword] = useState('');
 
+
+   // New state for user data
+   const [userData, setUserData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    university: '',
+    collegeDepartment: '',
+    collegeProgram: ''
+  });
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const user = auth.currentUser;
-        if (user) {
-          const userId = user.uid;
-          const dbRef = ref(getDatabase());
-          const snapshot = await get(child(dbRef, `users/${userId}`));
-
-          if (snapshot.exists()) {
-            const userDataFromDB = snapshot.val();
-            console.log("userDataFromDB:", userDataFromDB);
-            setUserData(userDataFromDB);
-          } else {
-            console.log("No data available");
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
+    // Set up a listener for authentication state changes
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("Authenticated user ID:", user.uid);
+        // Fetch user data from Firebase
+        fetchData(user.uid);
+      } else {
+        console.log("No user is currently logged in.");
       }
-    };
+    });
 
-    fetchUserData();
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
   }, []);
+
+  const fetchData = (userId) => {
+    const db = getDatabase();
+    const userRef = ref(db, `users/${userId}`);
+
+    get(userRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log("Fetched user data:", snapshot.val());
+        const data = snapshot.val();
+        setUserData({
+          firstName: data.firstName || '',
+          lastName: data.lastName || '',
+          email: data.email || '',
+          university: data.university || '',
+          collegeDepartment: data.collegeDepartment || '',
+          collegeProgram: data.collegeProgram || ''
+        });
+      } else {
+        console.log("No data available for user ID:", userId);
+      }
+    }).catch((error) => {
+      console.error("Firebase fetch error:", error);
+    });
+  };
+
+
+
 
   const handleMouseEnter = () => {
     setExpanded(true);
@@ -65,6 +87,20 @@ const ProfileView = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("Form submitted");
+  
+    if (auth.currentUser && password) {
+      console.log("Attempting to update password");
+      updatePassword(auth.currentUser, password)
+        .then(() => {
+          console.log("Password updated successfully!");
+        })
+        .catch((error) => {
+          console.error("Error updating password:", error);
+        });
+    } else {
+      console.log("User is not authenticated or password is empty");
+    }
   };
 
   return (
@@ -139,10 +175,9 @@ const ProfileView = () => {
           <Form.Group controlId="firstName">
               <Form.Label>First Name</Form.Label>
               <Form.Control
-             type="text"
-             value={userData.firstName}
-            
-                readOnly
+            type="text"
+            value={userData.firstName}
+            readOnly
               />
             </Form.Group>
 
@@ -193,13 +228,14 @@ const ProfileView = () => {
             </Form.Group>
 
             <Form.Group controlId="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="text"
-                value={userData.password}
-                readOnly
-              />
-            </Form.Group>
+  <Form.Label>Password</Form.Label>
+  <Form.Control
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    placeholder="Enter new password"
+  />
+</Form.Group>
 
             <Button variant="primary" type="submit">
               Update
